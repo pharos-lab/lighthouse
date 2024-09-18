0fd98a7fe070760d5e50a15f2377c2664c23203d
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-09-18 12:47:48 +0200

    made wantable structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,51 @@
+<template>
+  <Dashboard>
+    <Navbar>
+      <Link></Link>
+      <Link></Link>
+      <Link></Link>
+      <Actions>
+        <Action></Action>
+        <Action></Action>
+      </Actions>
+    </Navbar>
+    <Sidebar>
+      <Link></Link>
+      <Link></Link>
+      <Link></Link>
+      <Link></Link>
+      <Link></Link>
+      <Link></Link>
+    </Sidebar>
+    <Content>
+      <Stats>
+        <Stat></Stat>
+        <Stat></Stat>
+        <Stat></Stat>
+      </Stats>
+      
+      <Charts>
+        <Chart></Chart>
+        <Chart></Chart>
+        <Chart></Chart>
+      </Charts>
+      <Table></Table>
+      <List>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+      </List>
+    </Content>
+  </Dashboard>
+</template>
+
 <script setup>
+  
 </script>
 
-<template>
-</template>
+
 
 <style scoped>
 </style>
