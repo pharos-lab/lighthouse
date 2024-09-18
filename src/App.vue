482ebe3b1f735b683e1efdcd70662f7af0dd0914
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-09-18 10:52:59 +0200

    remove starter files (hello world component & assets)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <p class="bg-red-500">ok</p>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" class="bg-red-500"/>
 </template>
 
 <style scoped>
