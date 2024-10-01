1f92d40abfb393858c31899daf05e6d090c8f62e
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-10-01 09:32:09 +0200

    add color to dropdown links matches the navbar

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -2,8 +2,7 @@
     <div class="relative cursor-pointer">
         <div class="px-3 py-2" v-if="props.img" @click="showItems = !showItems">
             <img :src="props.img" 
-                :alt="props.alt || 'add alternative text'"  
-                class="bg-red-500" 
+                :alt="props.alt || 'add alternative text'" 
                 :class="[
                     props.type == 'action' ? 'h-8 w-8 rounded-full' : '',
                 ]"
@@ -13,15 +12,17 @@
             {{ props.label }}
         </button>
         
-        <div v-show="showItems" class="absolute bg-blue-500 right-0 rounded p-2">
+        <div v-show="showItems" class="absolute right-0 rounded p-2 top-16" :class="colorClass">
             <Link v-for="(link, index) in props.dropdown" v-bind="link" :key="index"/>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import Link from './Link.vue'
+import { backgroundColor } from './backgroundColor.js';
+
 
 const props = defineProps({
     label: String,
@@ -30,8 +31,28 @@ const props = defineProps({
     img: String,
     alt: String,
     href: String,
-    dropdown: Array
+    dropdown: Array,
+    color: {
+        type: String,
+        default: 'blue',
+        validator(value) {
+        // The value must match one of these strings
+        return ['gray', 'red', 'orange', 'yellow', 'green', 'blue'].includes(value);
+        },
+    },
+    mode: {
+        type: String,
+        default: 'fill',
+        validator(value) {
+        // The value must match one of these strings
+        return ['none', 'fill', 'light', 'outlined', 'text'].includes(value);
+        },
+    },
 })
+
+const colorClass = computed(() => {
+  return backgroundColor[props.color][props.mode];
+});
 
 const showItems = ref(false)
 </script>
